24128c44774c4de64510f347e7a4923bdd12f784
moaaskt <moacirmayer@hotmail.com>
2026-06-26 17:45:10 -0300

    chore(seed): add protected demo recovery seed"

--- backend/dist/pets/dto/update-pet.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreatePetDto } from './create-pet.dto';
-declare const UpdatePetDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreatePetDto>>;
-export declare class UpdatePetDto extends UpdatePetDto_base {
-}
-export {};
--- backend/dist/pets/entities/pet.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Pet = void 0;
-class Pet {
-}
-exports.Pet = Pet;
-//# sourceMappingURL=pet.entity.js.map
